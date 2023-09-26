21ea9406102aa071b182b0b33aa97995148e1671
AngeloGarcia <guioangelo.garcia@gmail.com>
2023-09-25 20:35:08 -0500

    avance1

--- app/src/main/java/com/cibertec/myapplication/SelectOptionAuthActivity.java
+++ app/src/main/java/com/cibertec/myapplication/SelectOptionAuthActivity.java
@@ -1,14 +1,25 @@
 package com.cibertec.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
+
 public class SelectOptionAuthActivity extends AppCompatActivity {
+
+
+    Toolbar  mToolbar ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_option_auth);
+
+        mToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("Seleccionar Opcion");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
